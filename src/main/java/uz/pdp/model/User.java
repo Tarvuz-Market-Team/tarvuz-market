8fb535bce4177e62ae130435c225fa69d2ed1fcb
Abror Asqarov <asqarovdanater@gmail.com>
2025-07-02 18:20:05 +0500

    UserService functionality. (#9)

--- src/main/java/uz/pdp/model/User.java
+++ src/main/java/uz/pdp/model/User.java
@@ -1,6 +1,22 @@
 package uz.pdp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import uz.pdp.base.BaseModel;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User extends BaseModel {
+    public enum userRole {
+        SELLER,
+        CUSTOMER,
+        ADMIN,
+    }
+
+    private String fullName;
+    private String username;
+    private String password;
+    private String userRole;
 }
